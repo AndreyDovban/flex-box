df700d46bb4578ef9c1deb3cbbb6a5250700be05
Andrey Dovban <dovban.andreyy@rambler.ru>
2024-10-13 12:25:18 +0300

    Реализован элемент списка со стрелочками

--- block/listBlock.go
+++ block/listBlock.go
@@ -3,7 +3,6 @@ package block
 import (
 	"flexbox/widgets"
 	"image/color"
-	"log"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/canvas"
@@ -19,40 +18,67 @@ func ListBlock() *fyne.Container {
 	hr := canvas.NewLine(color.White)
 	hr.Resize(fyne.NewSize(200, 0))
 
-	names := binding.NewStringList()
-	names.Set([]string{"Jon", "Bob", "Kate", "Frank", "Andrey"})
+	names := []string{"Jon", "Bob", "Kate", "Frank", "Andrey"}
+
+	target := binding.NewInt()
+	target.Set(-1)
+
+	showText := widget.NewLabel("")
 
 	addElem := widget.NewButton("Add elem", func() {
-		names.Append("New text")
+		names = append(names, "New text")
 	})
 
-	list := widget.NewListWithData(
-		names,
-		func() fyne.CanvasObject { return widget.NewLabel("") },
-		func(di binding.DataItem, co fyne.CanvasObject) {
-			txt, _ := di.(binding.String).Get()
-			co.(*widget.Label).SetText(txt)
+	list := widget.NewList(
+		func() int { return len(names) },
+		func() fyne.CanvasObject {
+			return widgets.NewArrowLabel("")
+		},
+		func(lii widget.ListItemID, co fyne.CanvasObject) {
+			targetId, _ := target.Get()
+
+			elem := co.(*widgets.ArrowLabel)
+			elem.Label.SetText(names[lii])
+
+			if targetId == lii {
+				elem.SelectedIcon.Show()
+			} else {
+				elem.SelectedIcon.Hide()
+			}
+
 		},
 	)
-	list.Resize(fyne.NewSize(200, 400))
+
 	list.OnSelected = func(id widget.ListItemID) {
-		s, _ := names.GetItem(id)
-		text, _ := s.(binding.String).Get()
-		log.Println(text)
+		target.Set(id)
+		list.RefreshItem(id)
+		showText.SetText(names[id])
 
 	}
+
+	list.HideSeparators = true
+
+	unselectBut := widget.NewButton("Unselect", func() {
+		showText.SetText("")
+		target.Set(-1)
+		list.UnselectAll()
+		list.FocusLost()
+
+	})
 
 	listContent := container.NewCenter(
 		container.NewStack(
 			widgets.Fon(color.Transparent, color.White, 8),
 			container.New(
-				layout.NewGridWrapLayout(fyne.NewSize(400, 400)),
+				layout.NewGridWrapLayout(fyne.NewSize(400, 500)),
 				container.New(layout.NewCustomPaddedLayout(16, 16, 16, 16),
 					container.New(layout.NewCustomPaddedVBoxLayout(24),
+						showText,
 						addElem,
+						unselectBut,
 						hr,
 						container.NewGridWrap(
-							fyne.NewSize(400-32, 260),
+							fyne.NewSize(400-32, 220),
 							list,
 						),
 					),
--- widgets/ArrowLabel.go
+++ widgets/ArrowLabel.go
@@ -11,24 +11,27 @@ import (
 	"fyne.io/fyne/v2/widget"
 )
 
+var arrowIconRed = &fyne.StaticResource{
+	StaticName:    "arrow-forward.svg",
+	StaticContent: []byte("<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\"><path d=\"M0 0h24v24H0z\" fill=\"none\"/><path fill=\"red\" d=\"M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z\"/></svg>\n"),
+}
+
 type ArrowLabel struct {
 	widget.BaseWidget
-	Icon *widget.Icon
-	Text string
-	F    fyne.CanvasObject
+	Icon  *widget.Icon
+	Label *widget.Label
+	F     fyne.CanvasObject
 
-	hovered bool
-
-	selected     bool
-	selectedIcon *widget.Icon
+	Selected     bool
+	SelectedIcon *widget.Icon
 }
 
 func NewArrowLabel(text string) *ArrowLabel {
 	elem := &ArrowLabel{
-		Text:         text,
+		Label:        widget.NewLabel(text),
 		Icon:         widget.NewIcon(theme.NavigateNextIcon()),
-		F:            container.NewGridWrap(fyne.NewSize(24, 24)),
-		selectedIcon: widget.NewIcon(theme.HistoryIcon()),
+		F:            container.NewGridWrap(fyne.NewSize(24, 0)),
+		SelectedIcon: widget.NewIcon(arrowIconRed),
 	}
 	elem.ExtendBaseWidget(elem)
 
@@ -41,7 +44,7 @@ func (elem *ArrowLabel) CreateRenderer() fyne.WidgetRenderer {
 	icon := elem.Icon
 	icon.Resize(fyne.NewSize(24, 24))
 
-	selIc := elem.selectedIcon
+	selIc := elem.SelectedIcon
 
 	c := container.NewStack(
 		Fon(color.Transparent, styles.Grey_400, 8),
@@ -52,7 +55,7 @@ func (elem *ArrowLabel) CreateRenderer() fyne.WidgetRenderer {
 				selIc,
 			),
 			nil,
-			widget.NewLabel(elem.Text),
+			elem.Label,
 		),
 	)
 	elem.Icon.Hide()
@@ -67,8 +70,7 @@ func (elem *ArrowLabel) Cursor() desktop.Cursor {
 
 // MouseIn is called when a desktop pointer enters the widget
 func (elem *ArrowLabel) MouseIn(*desktop.MouseEvent) {
-	elem.hovered = true
-	if !elem.selected {
+	if !elem.Selected {
 		elem.Icon.Show()
 		elem.Refresh()
 	}
@@ -80,18 +82,12 @@ func (elem *ArrowLabel) MouseMoved(*desktop.MouseEvent) {
 
 // MouseOut is called when a desktop pointer exits the widget
 func (elem *ArrowLabel) MouseOut() {
-	elem.hovered = false
 	elem.Icon.Hide()
 	elem.Refresh()
 }
 
-// func (elem *ArrowLabel) Tapped(*fyne.PointEvent) {
-// 	elem.Refresh()
-// 	if onTapped := elem.OnTapped; onTapped != nil {
-// 		if elem.selected {
-// 			elem.selectedIcon.Show()
-// 		}
-// 		onTapped()
-// 		elem.Refresh()
-// 	}
-// }
+func (elem *ArrowLabel) SetSelect() {
+	elem.Selected = true
+	elem.SelectedIcon.Show()
+	elem.Refresh()
+}
